232c2ad7f1b92cef845c7c17d39183fffdcf4473
knujella <knujella@verizonmedia.com>
2019-11-20 00:57:25 +0530

    Updates

---
+++ precache-manifest.c8d8d990e49dbff63ac693a56d5a7aee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e21e2fd3d4c7683fe632ced9053b2c",
+    "url": "/spider/index.html"
+  },
+  {
+    "revision": "ee8c36cbd6f09ab2921c",
+    "url": "/spider/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "34f63b7cc5c1ed00dbf0",
+    "url": "/spider/static/js/2.2060027d.chunk.js"
+  },
+  {
+    "revision": "ee8c36cbd6f09ab2921c",
+    "url": "/spider/static/js/main.97909dee.chunk.js"
+  },
+  {
+    "revision": "d6dbe8162d17903d1275",
+    "url": "/spider/static/js/runtime-main.d3990f0e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/spider/static/media/logo.25bf045c.svg"
+  }
+]);
